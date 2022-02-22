be2a7393eee01ef5a30e09dc71a8bc0efdd67ee1
TomG1993 <gorton.tom@hotmail.co.uk>
2022-02-22 14:32:10 +0000

    Added new todo functions

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
 import react, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToDoList from './ToDoList'
 
 const App = (props) => {
 
-  const [toDos, setToDos] = useState([]);
-
-  const [newTodo, setNewTodo] = useState("");
-
-  useEffect(()=>{
-    console.log("Working?")
-    GetTodos();
-  }, [])
-
-const GetTodos = async () =>{
-  const url = "https://localhost:7228/ToDo";
-  const response = await fetch(url,
-    {
-      mode: 'cors',
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    });
-    const data = await response.json();
-    
-    console.log(data);
-}
 
 
   return (
@@ -35,6 +12,7 @@ const GetTodos = async () =>{
       <header className="App-header">
       
       </header>
+      <ToDoList toDoList={toDos}/>
     </div>
   );
 }
