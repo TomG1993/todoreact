e7a6afc900374424482ead4efbfd26b1b2a7d641
TomG1993 <gorton.tom@hotmail.co.uk>
2022-02-23 11:26:57 +0000

    Update to stop empty todos and remove logging

--- src/ToDo.js
+++ src/ToDo.js
@@ -3,7 +3,6 @@ import StatusButton from './StatusButton'
  
 const ToDo = props => {
     const [toDo, setToDo] = useState(props);
-    console.log(toDo.todo)
    return (
        <div>
            {toDo.todo.description} - <StatusButton todo={toDo.todo} />
